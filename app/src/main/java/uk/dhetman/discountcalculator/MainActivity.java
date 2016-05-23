a130b64e58c53ad84283057f9ebac65be1fce41f
Dawid Hetmanczyk <dawid@asktheit.com>
2016-05-23 22:07:15 +0100

    First initial commit.

---
+++ app/src/main/java/uk/dhetman/discountcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.dhetman.discountcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
